836f4e34c3288e734622add3215b58b933a4b820
AmbroiseS <amb.soullier@gmail.com>
2017-02-19 02:12:49 +0100

    Added option to enable disable zoom on map with switch button using ng if

--- js/map.js
+++ js/map.js
@@ -1,3 +1,23 @@
 var app1=angular.module("mapApp",[])
 
-app1.controller("ardFilter", ['$scope',function($scope){
+
+app1.controller("switchButtons",['$scope', function($scope){
+
+
+	$scope.ifValue=false;
+
+	$scope.showIf = function() {
+		return $scope.ifValue;
+	};
+
+	$scope.hideIf = function() {
+		return !$scope.ifValue;
+	};
+
+
+}]);
+
+
+
+
+
