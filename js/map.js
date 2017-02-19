c0761688b926d3b4c90b7f26a714ff58ee5ec613
AmbroiseS <amb.soullier@gmail.com>
2017-02-19 16:27:36 +0100

    Cleaner implem of previous commit and allows to change parameters of url

--- js/map.js
+++ js/map.js
@@ -1,21 +1,74 @@
 var app1=angular.module("mapApp",[])
 
+var switcher=0;
+
+
+
 
 app1.controller("switchButtons",['$scope', function($scope){
+	
+	$scope.enableZoom= function(){
+		console.log(switcher);
+		if (switcher===1) {
+			document.getElementById('frameMap').src=updateUrl(document.getElementById('frameMap').src,'static','true');
+		} else{
+			document.getElementById('frameMap').src=updateUrl(document.getElementById('frameMap').src,'static','false');
 
+		}
 
-	$scope.ifValue=false;
-
-	$scope.showIf = function() {
-		return $scope.ifValue;
-	};
-
-	$scope.hideIf = function() {
-		return !$scope.ifValue;
-	};
-
+		switcher= switcher===0? 1 : 0;
+	}
 
 }]);
+
+
+
+
+//update, add or delete key and paramaters in an URL
+
+function updateUrl(url,key,value){
+	if(value!=undefined){
+		value = encodeURI(value);
+	}
+	var urls = url.split('?');
+	var baseUrl = urls[0];
+	var parameters = '';
+	var outPara = {};
+	if(urls.length>1){
+		parameters = urls[1];
+	}
+	if(parameters!=''){
+		parameters = parameters.split('&');
+		for(k in parameters){
+			var keyVal = parameters[k];
+			keyVal = keyVal.split('=');
+			var ekey = keyVal[0];
+			var eval = '';
+			if(keyVal.length>1){
+				eval = keyVal[1];
+			}
+			outPara[ekey] = eval;
+		}
+	}
+
+	if(value!=undefined){
+		outPara[key] = value;
+	}else{
+		delete outPara[key];
+	}
+	parameters = [];
+	for(var k in outPara){
+		parameters.push(k + '=' + outPara[k]);
+	}
+
+	var finalUrl = baseUrl;
+
+	if(parameters.length>0){
+		finalUrl += '?' + parameters.join('&'); 
+	}
+
+	return finalUrl; 
+}
 
 
 
