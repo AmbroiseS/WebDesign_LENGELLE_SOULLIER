9209bacaee55ca39b5d30703f7194050562a8975
AmbroiseS <amb.soullier@gmail.com>
2017-02-25 12:34:23 +0100

    Added accordeon, and example uses of graphs

--- js/stats.js
+++ js/stats.js
@@ -1,10 +1,12 @@
-angular.module("statsApp", ["chart.js"]).controller("ctrChart", function ($scope) {
+var app2=angular.module("statsApp", ["chart.js"])
+
+app2.controller("ctrChart", function ($scope) {
 
   $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
   $scope.series = ['Series A', 'Series B'];
   $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
+  [65, 59, 80, 81, 56, 55, 40],
+  [28, 48, 40, 19, 86, 27, 90]
   ];
   $scope.onClick = function (points, evt) {
     console.log(points, evt);
@@ -13,20 +15,31 @@ angular.module("statsApp", ["chart.js"]).controller("ctrChart", function ($scope
   $scope.options = {
     scales: {
       yAxes: [
-        {
-          id: 'y-axis-1',
-          type: 'linear',
-          display: true,
-          position: 'left'
-        },
-        {
-          id: 'y-axis-2',
-          type: 'linear',
-          display: true,
-          position: 'right'
-        }
+      {
+        id: 'y-axis-1',
+        type: 'linear',
+        display: true,
+        position: 'left'
+      },
+      {
+        id: 'y-axis-2',
+        type: 'linear',
+        display: true,
+        position: 'right'
+      }
       ]
     }
   };
+
+
+  $scope.labelsPie = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
+  $scope.dataPie = [300, 500, 100];
+
+
+
+
+
+
 });
-              
+
+
