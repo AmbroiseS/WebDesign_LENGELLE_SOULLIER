77327fd7f61b3099ecde603011bb7cc29b066ddb
AmbroiseS <amb.soullier@gmail.com>
2017-03-04 20:50:07 +0100

    Added values to pie chart

--- js/stats.js
+++ js/stats.js
@@ -17,8 +17,9 @@ app2.controller("ctrChart", function ($scope,$http) {
   value: 1
 };
 
-$scope.var50=5;
-$scope.var30=5;
+$scope.var50=0;
+$scope.var30=0;
+$scope.var15=0;
 
 
 $scope.getPieDataForArdt = function (ardt){
@@ -29,22 +30,61 @@ $scope.getPieDataForArdt = function (ardt){
   }).then(function successCallback(response) {
     $scope.var50=response.data.records.length;
   });
+  var temp30=0;
+  /*
   $http({
     method: 'GET',    
     url: 'https://opendata.paris.fr/api/records/1.0/search/?dataset=reseau-cyclable&rows=1000&facet=arrdt&facet=statut&facet=typologie&facet=sens_velo&refine.statut=VOIE+30&refine.arrdt='+ardt
 
   }).then(function successCallback(response) {
-    $scope.var30=response.data.records.length;
-    console.log("pie func" +$scope.var30+" "+ $scope.var50);
-  });
+   temp30+=response.data.records.length;
+   $scope.var30=temp30;
+   console.log("pie func" +$scope.var30+" "+ $scope.var50);
+ });*/
+ $http({
+  method: 'GET',    
+  url: 'https://opendata.paris.fr/api/records/1.0/search/?dataset=reseau-cyclable&rows=1000&facet=arrdt&facet=statut&facet=typologie&facet=sens_velo&refine.statut=ZONE+30&refine.arrdt='+ardt
+
+}).then(function successCallback(response) {
+  temp30+=response.data.records.length;
+  $scope.var30=temp30;
+});
+
+
+$http({
+  method: 'GET',    
+  url: 'https://opendata.paris.fr/api/records/1.0/search/?dataset=reseau-cyclable&rows=1000&facet=arrdt&facet=statut&facet=typologie&facet=sens_velo&refine.statut=VOIE+15&refine.arrdt='+ardt
+
+}).then(function successCallback(response) {
+ $scope.var15=response.data.records.length;
+});
+
+var tempPiet=0;
+$http({
+  method: 'GET',    
+  url: 'https://opendata.paris.fr/api/records/1.0/search/?dataset=reseau-cyclable&rows=1000&facet=arrdt&facet=statut&facet=typologie&facet=sens_velo&refine.statut=AIRE+PIETONNE&refine.arrdt='+ardt
+
+}).then(function successCallback(response) {
+ tempPiet+=response.data.records.length;
+ $scope.varPiet=tempPiet;
+});
+$http({
+  method: 'GET',    
+  url: 'https://opendata.paris.fr/api/records/1.0/search/?dataset=reseau-cyclable&rows=1000&facet=arrdt&facet=statut&facet=typologie&facet=sens_velo&refine.statut=ZONE+DE+RENCONTRE&refine.arrdt='+ardt
+
+}).then(function successCallback(response) {
+ tempPiet+=response.data.records.length;
+ $scope.varPiet=tempPiet;
+ $scope.labelsPie = ["Voies 50", "Voies 30 et Zones 30", "Voies 15","Zones Piétonnes et Partagées"];
+ $scope.dataPie = [$scope.var50, $scope.var30, $scope.var15,$scope.varPiet];
+});
 
 
 
-
-  $scope.labelsPie = ["Voies 50", "Voies 30"];
-  $scope.dataPie = [$scope.var50, $scope.var30];
 
 }
+
+
 
 });
 
